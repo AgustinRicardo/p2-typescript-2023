e8215a91a1daa98ef0758bd4db81daa94ac4cfa4
Pau Fernández <pau.fernandez@upc.edu>
2023-04-18 13:35:02 +0200

    Los botones ya responden

--- functions.js
+++ functions.js
@@ -1,11 +1,10 @@
-//const button = document.getElementById('btn');
-//
-//button?.addEventListener('click', function handleClick(event) {
-//  console.log('button clicked');
-//  console.log(event);
-//  console.log(event.target);
-//});
-
-function fn(){
-  alert("Alerta")
-}
+// Hay que poner un handler de click a TODOS los botones!
+// Y para pillarlos todos es mejor usar 'querySelectorAll'
+document.querySelectorAll('.user .data button').forEach((button) => {
+  // Usamos el DOM para recuperar el nombre del personaje
+  const name = button.parentElement.querySelector('.data .name');
+  // Ponemos el handler para clicks, que muestra el nombre en la consola
+  button.addEventListener('click', () => {
+    console.log("Has clicado: ", name.textContent);
+  });
+});
